7d3daecd2b7f2b1a6a04fe5c1de4eea5055d0ee1
aminelli <56305052+aminelli@users.noreply.github.com>
2024-05-30 16:07:57 +0200

    Update backups.bat

--- ES002/backups.bat
+++ ES002/backups.bat
@@ -1,3 +1,3 @@
 @echo off
-docker exec ms002-postgres_primary-1  /bin/bash
+docker exec -it ms002-postgres_primary-1  /bin/bash
 pg_basebackup -D /tmp/backups/B001 -F tar -z -P -v -X fetch -h localhost -U user
