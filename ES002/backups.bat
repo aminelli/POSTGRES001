bb9f26ab68228dd9e9f0560972272d4de6b9e0a5
aminelli <56305052+aminelli@users.noreply.github.com>
2024-05-30 17:23:13 +0200

    gfh

--- ES002/backups.bat
+++ ES002/backups.bat
@@ -1,3 +1,4 @@
 @echo off
-docker exec -it ms002-postgres_primary-1  /bin/bash
+
+docker exec -it ms002-postgres_primary-1 /bin/bash
 pg_basebackup -D /tmp/backups/B001 -F tar -z -P -v -X fetch -h localhost -U user
